b31330b1afee34a052e0772420944433d8e459db
Yutaka Yamada <y2squared@gmail.com>
2015-12-13 17:26:46 +0900

    add utf-8 setting

--- w2v/niconico/nicocrawl/management/commands/add_active_query.py
+++ w2v/niconico/nicocrawl/management/commands/add_active_query.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 -*-
 from django.core.management.base import BaseCommand, CommandError
 from django.db import DEFAULT_DB_ALIAS, connections
 from nicocrawl.models import Query, Queryhistory
